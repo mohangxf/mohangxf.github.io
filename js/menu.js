0fa03d91f9002eedccdc393d7c0c4357988094e3
mohangxf <136475997+mohangxf@users.noreply.github.com>
2023-06-16 23:05:01 +0530

    apps 1st post

--- js/menu.js
+++ js/menu.js
@@ -23,3 +23,31 @@
 		menu.addEventListener('transitionend', removeMenuTransition, false);
 	}
 }(document, window));
+
+
+// Download Button START
+
+// Get the necessary elements
+const generateBtn = document.getElementById("generateBtn");
+const progressBar = document.getElementById("progressBar");
+const successText = document.getElementById("successText");
+const downloadBtn = document.getElementById("downloadBtn");
+
+// Function to simulate the download generation process
+function generateDownloadLink() {
+	generateBtn.style.display = "none"; // Hide the generate download link button
+	progressBar.style.display = "block"; // Show the progress bar
+
+	// Simulate a 15-second process
+	setTimeout(() => {
+		progressBar.style.display = "none"; // Hide the progress bar
+		successText.style.display = "block"; // Show the success text
+		downloadBtn.style.display = "block"; // Show the download button
+	}, 15000);
+}
+
+// Event listener for the generate download link button
+generateBtn.addEventListener("click", generateDownloadLink);
+
+
+// Download Button END
